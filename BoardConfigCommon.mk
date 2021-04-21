c1ec6ed8de45b9ecdfc2fa493142e2be024a2807
Sohil876 <sohil876@gmail.com>
2021-04-21 12:55:52 +0000

    msm8953-common: Enable AOSP surfaceflinger

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,6 +174,9 @@ endif
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HOSTAPD_DRIVER := NL80211
